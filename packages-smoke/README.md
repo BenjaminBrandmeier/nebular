581a542ebe6f24fe73557ed491b9c93452d77887
dragonbane0 <marwinmisselhorn@hotmail.de>
2019-06-29 17:54:04 +0200

    Release theme for ootr-gui

--- packages-smoke/README.md
+++
@@ -1,4 +0,0 @@
-# BuildSmoke
-
-This project is used to verify all nebular packages were built properly.
-We just install newly build packages in this app and build it in production mode.
--- scripts/gulp/gulpfile.ts
+++
@@ -1,9 +0,0 @@
-import { task } from 'gulp';
-
-import './tasks/inline-resources/inline-resources';
-import './tasks/bundle/bundle';
-import './tasks/docs/docs';
-import './tasks/copy-sources';
-import './tasks/bump-versions';
-
-task('default', ['copy-sources']);
